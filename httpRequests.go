05162f5ab7cbf8d627ba5b533475af6d382a406d
brightDN <brightondeneef@gmail.com>
2025-08-24 22:46:09 +0200

    Introduced the MAP functionality

---
+++ httpRequests.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"net/http"
+	"time"
+	"io"
+	"fmt"
+	"encoding/json"
+)
+
+func commandMap(cfg *config) error{
+
+	url := "https://pokeapi.co/api/v2/location-area/"
+    if cfg.nextLocationURL != nil {
+        url = *cfg.nextLocationURL
+    }
+
+	if cfg.nextLocationURL == nil && cfg.previousLocationURL != nil {
+		fmt.Println("You are on the last page")
+		return nil
+	}
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	
+	var response LocationAreaResponse
+	if err := json.Unmarshal(body, &response); err != nil {
+    	return fmt.Errorf("Error unmarshalling JSON: %v", err)
+	}
+
+	for _, location := range response.Results {
+    	fmt.Println(location.Name)
+	}
+
+    cfg.nextLocationURL = response.Next
+    cfg.previousLocationURL = response.Previous
+	return nil
+}
+
+type PokeLocation struct
+{
+	Name string `json:"name"`
+	Url string `json:"url"`
+}
+
+type LocationAreaResponse struct {
+    Results 	[]PokeLocation	`json:"results"`
+	Next 		*string 		`json:"next"`
+	Previous	*string			`json:"previous"`
+}
--- main.go
+++ main.go
@@ -8,23 +8,36 @@ import (
 )
 
 func main() {
+	initCommands()
+	cfg := &config{} 
 	scanner := bufio.NewScanner(os.Stdin)
-	
+	commandFound := false
+
 	for {
-		fmt.Print("Pokedéx > ")
+		fmt.Print("Pokedex > ")
 
 		if !scanner.Scan() {
 			fmt.Println("Something went wrong, exiting program.")
 			break
 		}
 
-		cleanInput := CleanInput(scanner.Text())
-		if len(cleanInput) == 0 {
-			fmt.Println("Empty command detected!")
+		if len(scanner.Text()) == 0 {
 			continue
 		}
 
-		fmt.Printf("Your command was: %s\n", cleanInput[0])
+		cleanInput := CleanInput(scanner.Text())
+
+		commandFound = false
+		for _, command := range supportedCommands {
+			if cleanInput[0] == command.name {
+				commandFound = true
+				command.callback(cfg)
+				break
+			}
+		}
+		if !commandFound {
+			fmt.Println("Unknown command")
+		}
 	}
 }
 
@@ -32,4 +45,53 @@ func CleanInput(text string) []string {
 	lowerCased := strings.ToLower(text)
 	trimmed := strings.TrimSpace(lowerCased)
 	return strings.Fields(trimmed)
+}
+
+func commandExit(cfg *config) error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp(cfg *config) error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:\n")
+
+	for _, command := range supportedCommands {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
+}
+
+var supportedCommands map[string]cliCommand
+
+func initCommands() {
+    supportedCommands = map[string]cliCommand{
+        "exit": {
+            name:        "exit",
+            description: "Exit the program",
+            callback:    commandExit,
+        },
+        "help": {
+            name:        "help", 
+            description: "Displays a help message",
+            callback:    commandHelp,
+        },
+        "map": {
+            name:        "map",
+            description: "Show the next 20 locations", 
+            callback:    commandMap,
+        },
+    }
+}
+
+type cliCommand struct {
+	name			string
+	description 	string
+	callback func(*config) error
+}
+
+type config struct {
+    nextLocationURL     *string
+    previousLocationURL *string
 }
